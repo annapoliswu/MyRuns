47524562c0221baf59c124cf5b16cf4b8a5403a4
Anna Wu <annapoliswu@gmail.com>
2021-09-23 00:44:59 -0400

    finally figured out settings fragment and preference layout

--- app/src/main/java/com/zw/myruns/MainActivity.kt
+++ app/src/main/java/com/zw/myruns/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.preference.PreferenceFragmentCompat
 
 
 //NOTE don't need to crop image for this class
--- app/src/main/java/com/zw/myruns/SettingsFragment.kt
+++ app/src/main/java/com/zw/myruns/SettingsFragment.kt
@@ -10,17 +10,23 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        //TODO: createprefs
     }
 
+
+    /*
+    //MUST INFLATE OWN VIEW OR CRASHES
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         return view
     }
+    */
 
 }
